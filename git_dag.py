e48099859f11965833f607c709301b68783b2305
Michael Landis <mlandis@gmail.com>
2014-01-22 16:18:40 -0800

    revamped how events are counted

--- git_dag.py
+++ git_dag.py
@@ -29,10 +29,11 @@ class GitGraph(object):
     def __init__(self):
         self.nodes = {}
         self.count = {}
-        self.gain = {}
-        self.loss = {}
+        self.change = {}
+        self.time_list = []
+        self.count_list = []
 
-    def make_graph(self,fp='.',in_days=True):
+    def get_git_log(self,fp='.'):
         '''
         Populates GitGraph using GitNode objects.
         Reads git-log output from directory at fp.
@@ -41,49 +42,79 @@ class GitGraph(object):
         os.chdir(fp)
         cmd_str = 'git log --date=raw --pretty=format:\"%h,%p,%ad,%s\"'
         lines = subprocess.check_output(cmd_str.split(' ')).splitlines()
-        lines.reverse()
 
-        n_branches = 0
-        n_gain = 0
-        n_loss = 0
-        
+        # makes dict of t-indexed gitlog tokens 
+        t_args = {}
         for l in lines:
-            nd_args = l.strip('\"').split(',')
-            #print nd_args
+            print l
+            args = l.strip('\"').split(',')
+            t_tok = args[2].split(' ')
+            t = int(t_tok[0]) # raw time (in s) 
+            t += int(t_tok[1])*3600 # correct for timezone 
+            args[2] = t
+            args[3] = ','.join(args[3:]) 
+            t_args[t] = args[0:4]
+
+        os.chdir(cwd)
+        return t_args
+
+
+    def make_graph(self,git_log={},fp='.',in_days=True):
+        '''
+        Populates GitGraph using GitNode objects.
+        Reads git-log output from directory at fp.
+        '''
+
+        if git_log == {}:
+            t_args = self.get_git_log(fp)
+        else:
+            t_args = git_log
+      
+        for t in sorted(t_args.keys()):
             
-            nd_sha = nd_args[0]
-            nd_t = float(nd_args[2].split(' ')[0])
-            print nd_t
+            nd_sha,pa_tok,nd_t,nd_msg = t_args[t]
             if in_days:
                 nd_t /= 86400.
-            nd_msg = nd_args[3]
 
             # add new nodes to graph
+            dn = 0
             if nd_sha not in self.nodes:
                 self.nodes[nd_sha] = GitNode(nd_sha,nd_t,nd_msg)
-                n_branches += 1
-                n_gain += 1
+                dn = 1
+            nd = self.nodes[nd_sha]
 
             # update parent/child relationship
-            if nd_args[1] != '':
-                pa_list = nd_args[1].split(' ')
+            if pa_tok != '':
+                pa_list = pa_tok.split(' ')
+
+                # add missing parents to current time 
                 for pa_sha in pa_list:
-                    self.nodes[nd_args[0]].add_parent(self.nodes[pa_sha])
-                    self.nodes[pa_sha].add_child(self.nodes[nd_args[0]])
+                    if pa_sha not in self.nodes:
+                        self.nodes[pa_sha] = GitNode(pa_sha,nd_t,nd_msg)
+
+                for pa_sha in pa_list:
+                    nd.add_parent(self.nodes[pa_sha])
+                    self.nodes[pa_sha].add_child(nd)
 
                 if len(self.nodes[pa_sha].children) == 1:
-                    n_branches -= 1
-                    n_gain -= 1
+                    dn = 0
 
                 if len(self.nodes[nd_sha].parents) > 1:
-                    n_branches -= 1
-                    n_loss += 1
+                    dn = -len(self.nodes[nd_sha].parents)+1
+            else:
+                dn = 0
 
             # update counts
-            self.count[nd_t] = n_branches
-            self.gain[nd_t] = n_gain
-            self.loss[nd_t] = n_loss
-        os.chdir(cwd)
+            self.change[nd_t] = dn
+
+        n = 1
+        for i,nd_t in enumerate(sorted(self.change.keys())):
+            dn = self.change[nd_t]
+            n += dn
+            self.count[nd_t] = n
+            self.count_list.append(n)
+            self.time_list.append(nd_t)
+
 
     def llik(self,args=[.1,.1,.1]):
         '''
@@ -92,40 +123,43 @@ class GitGraph(object):
         All branches undergo events B,M,C at rates r_B,r_M,r_C respectively
         '''
         
-        x=self.count
         branch_rate,merge_rate,commit_rate=args
         
         llik = 0.
         old_n = 1
         old_t = 0.
-        T = sorted(x.keys())
-        for i,t in enumerate(T):
+        for i,t in enumerate(self.time_list):
             if i == 0:
                 old_t = t
                 continue
+            
+            n = self.count_list[i]
+            dn = n - old_n
+            dt = t - old_t
+            if dt == 0:
+                old_n = n
+                continue
 
-            n = x[t]
             if n > old_n: r = branch_rate
             elif n < old_n: r = merge_rate
             else: r = commit_rate
 
             if n > 1: 
-                llik += scipy.log(r) - n * (branch_rate + merge_rate + commit_rate) * (t - old_t)
+                llik += scipy.log(r) - n * (branch_rate + merge_rate + commit_rate) * dt
             else:
-                llik += scipy.log(r) - n * (branch_rate + commit_rate) * (t - old_t)
+                llik += scipy.log(r) - n * (branch_rate + commit_rate) * dt
 
-            #print n,t-old_t,branch_rate,merge_rate,commit_rate,llik
-
+            #print dt,dn,llik
             old_n = n 
             old_t = t
-        
+
         return(llik)
 
     def find_mle(self):
-        o = scipy.optimize.fmin_l_bfgs_b(func=self.llik,x0=scipy.stats.expon.rvs(.1,size=3),bounds=[(1e-9,None)]*3,approx_grad=True,factr=10.,epsilon=.0001,pgtol=1e-30)
+        o = scipy.optimize.fmin_l_bfgs_b(func=self.llik,x0=scipy.stats.expon.rvs(.05,size=3),bounds=[(1e-9,None)]*3,approx_grad=True,factr=10.,epsilon=.0001,pgtol=1e-30)
         return o
 
-    def run_mcmc(self,n=5000,prior=[10.]*3,proposal_tune=[1.]*3,thin=10,burn=0.2,fn='mcmc.txt',stdout=True):
+    def run_mcmc(self,n=5000,prior=[1.]*3,proposal_tune=[1.]*3,thin=10,burn=0.2,fn='mcmc.txt',stdout=True):
         '''
         GitGraph.run_mcmc runs MCMC on GitGraph using GitGraph.llik
         '''
